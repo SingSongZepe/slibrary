4dd084737fe5fa6db7d1c067dca0907cc2af5656
SingSongZep_ <103307771+SingSongZepe@users.noreply.github.com>
2023-10-13 20:17:33 +0800

    Add files via upload

---
+++ value/value.py
@@ -0,0 +1,26 @@
+# toggle bar
+lb_selected_radius = 2
+
+# book nums 
+book_nums_default = 15
+
+# for downloading unfair
+unfair_limit = 10 * 1024
+
+# for showing of book
+columns = 3
+
+# place of widget a book item
+spacing_gl_result = 5
+height_book_item = 120
+book_item_radius = 5
+    # position of wgt_main
+ax = 5
+ay = 5
+
+# for showing of history item (search or book)
+history_columns = 2
+spacing_gl_history = 10
+height_history_item = 120
+history_item_radius = 5
+
